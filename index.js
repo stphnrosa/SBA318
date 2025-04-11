404e5956d97413030f6dc566257299542202d419
stphnrosa <stphn.rosa@gmail.com>
2025-04-11 14:46:22 -0400

    added

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 const express = require("express")
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-app.get('/', () => {
-  
+app.get('/', (req,res) => {
+  res.send('The "home" route of my express server')
 })
+
+app.get('/home', (req,res) => {
+  res.send('<h1> Hello World </h1>')
+})
+
+app.get('/list', (req,res) => {
+  res.send(`
+     <ul>
+        <li> Item 1 </li>
+        <li> Item 2 </li>
+        <li> Item 3 </li>
+      </ul>
+    `);
+    //
+})
+
+app.get('/go-home',(req,res) => {
+  res.redirect('/home');
+})
+
+app.get('/json', (req,res) => {
+  res.json({
+       learner: '',
+       grades:[90,80],
+       enrolled:true,
+       course: 'NodeJS and Express'
+  })
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT: ${PORT}`)
+})
+
