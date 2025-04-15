2dbf4e217e92e00a56f4d039147f1a4a0fdaabf4
stphnrosa <stphn.rosa@gmail.com>
2025-04-14 20:06:21 -0400

    added skincare info

--- server.js
+++ server.js
@@ -5,28 +5,23 @@
 
 
 const express = require("express");
-const expressLayouts = require('express-ejs-layouts')
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const PORT = 3000;
-
-
-////////////////////////////////////////////////////
-// serve static files from the styles dictionary //
-///////////////////////////////////////////////////
-app.use(express.static('public'));
-app.use('/css', express.static(__dirname + 'public/css'))
 
 
 ////////////////////////////////////////////
 ///////////// TEMPLATE ENGINE /////////////
 //////////////////////////////////////////
-app.use(expressLayouts)
+app.use(expressLayouts);
 app.set('view engine', 'ejs'); // installed this by typing npm install ejs in terminal
-app.set('views', './views');
+app.set('layout', 'layouts/layout');
+app.use(express.static('public'));
 
-const fs = require("fs"); // lets you read/create/update/delete/rename files
 
-//middleware should be at the top of the file
+///////////////////////////////////////////////
+//middleware should be at the top of the file//
+///////////////////////////////////////////////
 app.use((req, res, next) => {
   console.log("This middleware is running for all routes. This one runs first");
   next(); // this lets the code know to run whatever is next
@@ -39,12 +34,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((err, req, res, next) => {
-  // This includes the 4 arguments needed
-  console.error(err.stack);
-  res.status(500).send("Uh-oh! There's been an error");
-}); // Should I leave error handling middleware here or after routes? Not sure.
-
 function customMiddleware(req, res, next) {
   console.log(`This middleware runs for all routes. this is the third`);
   next();
@@ -52,36 +41,28 @@ function customMiddleware(req, res, next) {
 
 app.use(customMiddleware);
 
+
 //////////////////////////////////////////////////////
 //////////////// ROUTES !!!!! ////////////////////////
 /////////////////////////////////////////////////////
 
 app.get("/", (req, res) => {
-  res.send('The "home" route of my express server');
+  res.render('home', { title: 'Home' });
 });
 
-app.get("/home", (req, res) => {
-  res.send("<h1> Hello World </h1>");
-});
-
-app.get("/list", (req, res) => {
-  res.send(`
-     <ul>
-        <li> Item 1 </li>
-        <li> Item 2 </li>
-        <li> Item 3 </li>
-      </ul>
-    `);
-  //
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About',
+    items: ['Item 1', 'Item 2', 'Item 3']
+  });
 });
 
 app.get("/go-home", (req, res) => {
   res.redirect("/home");
 });
 
-app.get("/Posts", (req, res) => {
-  res.send("<h1> Here are some of our favorite skincare products </h1>");
-  res.send()
+app.get("/posts", (req, res) => {
+  res.render('posts', { title: 'posts' });
 });
 
 app.get("/json", (req, res) => {
@@ -105,6 +86,19 @@ app.get("/name/:firstName", (req, res) => {
   console.log("Queries", req.query);
   res.send(`My name is ${req.params.firstName} ${req.query.lastName}`);
 }); // key value pair in URL uses ? and =
+
+//////////////////////Error Handling/////////////////////////
+////////////////////////////////////////////////////////////
+
+app.use((err, req, res, next) => {
+  // This includes the 4 arguments needed
+  console.error(err.stack);
+  res.status(500).send("Uh-oh! There's been an error");
+}); // Should I leave error handling middleware here or after routes? Not sure.
+
+///////////////////////////////////////////////
+//////////////////LISTENING///////////////////
+//////////////////////////////////////////////
 
 app.listen(PORT, () => {
   console.log(`Server is listening on PORT: ${PORT}`);
